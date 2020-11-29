7333406409190b46a57c5e16a43458fb153632a7
Refayat Haque <rhaque@pyramidsystems.com>
2020-11-29 00:28:16 -0500

    Nov 29 028

--- modules.tf
+++ modules.tf
@@ -1,6 +1,6 @@
 module "consul" {
   source   = "github.com/wardviaene/terraform-consul-module.git?ref=terraform-0.12"
-  key_name = "mykey"
+  key_name = aws_key_pair.mykey.key_name
   key_path = var.PATH_TO_PRIVATE_KEY
   region   = var.AWS_REGION
   vpc_id   = aws_default_vpc.default.id
