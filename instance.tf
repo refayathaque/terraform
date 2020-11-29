c2b6d84213779eed089eaf1b3d7fad07ba74a015
Refayat Haque <rhaque@pyramidsystems.com>
2020-11-29 01:26:31 -0500

    Nov 29 126

--- instance.tf
+++ instance.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "mykey" {
-  key_name   = "mykey"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
-}
-
 resource "aws_instance" "example" {
   # ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   ami           = var.AMIS[var.AWS_REGION]
