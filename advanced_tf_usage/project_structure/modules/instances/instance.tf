a40ce4988b5e1d7c96f42434ff699b702ae08020
Refayat Haque <rhaque@pyramidsystems.com>
2020-12-25 22:58:48 -0500

    Dec 25 2258

--- advanced_tf_usage/project_structure/modules/instances/instance.tf
+++ advanced_tf_usage/project_structure/modules/instances/instance.tf
@@ -1,20 +1,8 @@
-variable "ENV" {
-}
-
-variable "INSTANCE_TYPE" {
-  default = "t2.micro"
-}
-
-variable "PUBLIC_SUBNETS" {
-  type = list
-}
-
-variable "VPC_ID" {
-}
-
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
-}
+variable "ENV" {}
+variable "INSTANCE_TYPE" { default = "t2.micro" }
+variable "PUBLIC_SUBNETS" { type = list }
+variable "VPC_ID" {}
+variable "PATH_TO_PUBLIC_KEY" { default = "mykey.pub" }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
